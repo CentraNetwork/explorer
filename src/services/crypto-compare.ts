47976d7ff3c1787433307eb7fe6f96479f299d4c
CentraDev <57597157+CentraDev@users.noreply.github.com>
2019-11-10 12:57:08 -0500

    Update crypto-compare.ts

--- src/services/crypto-compare.ts
+++ src/services/crypto-compare.ts
@@ -1,161 +1,133 @@
-import axios from "axios";
-import moment from "moment";
-import store from "@/store";
-
-const SECONDS_PER_DAY = 86400;
-
-// CryptoCompare supports upto 20 requests per second
-const MAX_REQUESTS_PER_SECOND = 1;
-const REQUEST_INTERVAL = 50;
-
-const limiter = axios.create({});
-
-limiter.interceptors.request.use(config => {
-  return new Promise((resolve, reject) => {
-    const interval = setInterval(() => {
-      if (CryptoCompareService.pendingRequests < MAX_REQUESTS_PER_SECOND) {
-        CryptoCompareService.pendingRequests++;
-        clearInterval(interval);
-        resolve(config);
-      }
-    }, REQUEST_INTERVAL);
-  });
-});
-
-limiter.interceptors.response.use(
-  response => {
-    CryptoCompareService.pendingRequests = Math.max(0, CryptoCompareService.pendingRequests - 1);
-    return Promise.resolve(response);
-  },
-  error => {
-    CryptoCompareService.pendingRequests = Math.max(0, CryptoCompareService.pendingRequests - 1);
-    return Promise.reject(error);
-  },
-);
+import axios from 'axios'
+import moment from 'moment'
+import store from '@/store'
 
 class CryptoCompareService {
-  public static pendingRequests: number = 0;
+  async price(currency) {
+    const response = await axios.get(`https://apiv2.bitz.com/Market/coinRate?coins=xph`)
+    const key = currency.toLowerCase()
 
-  public async get(url: string, options?: any) {
-    return limiter.get(url, options);
-  }
-
-  public async price(currency: string): Promise<number | undefined> {
-    const response = await this.get(`https://min-api.cryptocompare.com/data/price?fsym=ARK&tsyms=${currency}`);
-    if (response.data[currency]) {
-      return Number(response.data[currency]);
+    if (response.data.data.xph && response.data.data.xph.hasOwnProperty(key)) {
+      return Number(response.data.data.xph[key])
     }
   }
 
-  public async day(): Promise<{ datasets: any; labels: any }> {
-    return this.sendRequest("hour", 24, "HH:mm");
+  async day() {
+    return this.sendRequest('hour', 24, 'HH:mm')
   }
 
-  public async week(): Promise<{ datasets: any; labels: any }> {
-    return this.sendRequest("day", 7, "DD.MM");
+  async week() {
+    return this.sendRequest('day', 7, 'DD.MM')
   }
 
-  public async month(): Promise<{ datasets: any; labels: any }> {
-    return this.sendRequest("day", 30, "DD.MM");
+  async month() {
+    return this.sendRequest('day', 30, 'DD.MM')
   }
 
-  public async quarter(): Promise<{ datasets: any; labels: any }> {
-    return this.sendRequest("day", 120, "DD.MM");
+  async quarter() {
+    return this.sendRequest('day', 120, 'DD.MM')
   }
 
-  public async year(): Promise<{ datasets: any; labels: any }> {
-    return this.sendRequest("day", 365, "DD.MM");
+  async year() {
+    return this.sendRequest('day', 365, 'DD.MM')
   }
 
-  public async sendRequest(
-    type: string,
-    limit: number,
-    dateTimeFormat: string,
-  ): Promise<{ datasets: any; labels: any }> {
-    const date = Math.round(new Date().getTime() / 1000);
-    const token = store.getters["network/token"];
+  async sendRequest(type, limit, dateTimeFormat) {
+    const date = Math.round(new Date().getTime() / 1000)
 
-    let targetCurrency = "USD";
-    if (store.getters["currency/name"] !== token) {
-      targetCurrency = store.getters["currency/name"];
+    let targetCurrency = 'USD'
+    if (store.getters['currency/name'] !== store.getters['network/token']) {
+      targetCurrency = store.getters['currency/name']
     }
 
-    const response = await this.get(`https://min-api.cryptocompare.com/data/histo${type}`, {
-      params: {
-        fsym: token,
-        tsym: targetCurrency,
-        toTs: date,
-        limit,
-      },
-    });
-
-    return this.transform(response.data.Data, dateTimeFormat);
+    const response = await axios
+      .get(`https://min-api.cryptocompare.com/data/histo${type}`, {
+        params: {
+          fsym: store.getters['network/token'],
+          tsym: targetCurrency,
+          toTs: date,
+          limit
+        }
+      })
+    return this.transform(response.data.Data, dateTimeFormat)
   }
 
-  public async dailyAverage(timestamp: number): Promise<number | null> {
-    const networkAlias = store.getters["network/alias"];
-    if (networkAlias !== "Main") {
-      return null;
+  async dailyAverage(timestamp) {
+    const networkAlias = store.getters['network/alias']
+    if (networkAlias !== 'Main') {
+      return null
     }
 
-    let ts = moment.unix(timestamp).unix();
+    let ts = moment()
+      .utc()
+      .set({
+        year: 2019,
+        month: 0,
+        date: 3,
+        hour: 12,
+        minute: 0,
+        second: 0
+      })
+      .add(timestamp, 'seconds')
+      .unix()
 
     // get last second of the day as unix timestamp
-    ts = ts - (ts % SECONDS_PER_DAY) + SECONDS_PER_DAY - 1;
+    ts = ts - (ts % 86400) + 86400 - 1
 
-    const targetCurrency = store.getters["currency/name"];
-    const lastConversion = store.getters["currency/lastConversion"];
+    const targetCurrency = store.getters['currency/name']
+    const lastConversion = store.getters['currency/lastConversion']
 
     if (lastConversion.to === targetCurrency && lastConversion.timestamp === ts) {
-      return lastConversion.rate;
+      return lastConversion.rate
     }
 
-    const token = store.getters["network/token"];
-    const cache = JSON.parse(localStorage.getItem(`rates_${targetCurrency}`) as string);
+    const token = store.getters['network/token']
+    const cache = JSON.parse(localStorage.getItem(`rates_${targetCurrency}`))
 
-    if (cache && cache[ts]) {
-      store.dispatch("currency/setLastConversion", {
+    if (cache && cache.hasOwnProperty(timestamp)) {
+      store.dispatch('currency/setLastConversion', {
         to: targetCurrency,
-        timestamp: ts,
-        rate: cache[ts],
-      });
+        timestamp: timestamp,
+        rate: cache[timestamp]
+      })
 
-      return Number(cache[ts]);
+      return cache[timestamp]
     }
 
-    const response = await this.get("https://min-api.cryptocompare.com/data/dayAvg", {
-      params: {
-        fsym: token,
-        tsym: targetCurrency,
-        toTs: ts,
-      },
-    });
+    const response = await axios
+      .get('https://min-api.cryptocompare.com/data/dayAvg', {
+        params: {
+          fsym: token,
+          tsym: targetCurrency,
+          toTs: ts
+        }
+      })
 
-    if (response.data.Response === "Error") {
-      return null;
+    if (response.data.Response === 'Error') {
+      return null
     }
 
-    const rate = response.data[targetCurrency];
+    const rate = response.data[targetCurrency]
 
-    store.dispatch("currency/setLastConversion", {
+    store.dispatch('currency/setLastConversion', {
       to: targetCurrency,
       timestamp: ts,
-      rate,
-    });
+      rate: rate
+    })
 
-    return rate;
+    return rate
   }
 
-  public transform(response: any, dateTimeFormat: string): { datasets: any; labels: any } {
+  transform(response, dateTimeFormat) {
     return {
-      labels: response.map((value: any) => {
-        return moment.unix(value.time).format(dateTimeFormat);
+      labels: response.map(value => {
+        return moment.unix(value.time).format(dateTimeFormat)
       }),
-      datasets: response,
-    };
+      datasets: response.map(value => {
+        return value.close
+      })
+    }
   }
 }
 
-CryptoCompareService.pendingRequests = 0;
-
-export default new CryptoCompareService();
+export default new CryptoCompareService()
